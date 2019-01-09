c831e2fa2e0afe61777d88689481d679e1d32de7
Douglas Denner <doh_guedes@hotmail.com>
2019-01-09 17:38:54 -0200

    inicio

---
+++ class/sql.php
@@ -0,0 +1,35 @@
+<?php
+class sql extends PDO {
+    private $conn;
+
+    public function __construct(){
+  	     $this->conn = new PDO("mysql:host=localhost;dbname=bdphp7","root","");
+
+  }
+    private function setparams ($statments, $parameters = array ()){
+
+   	  foreach ($parameters as $key =>$values){
+	  	$this->setparam($key, $values);
+	  }
+   }
+
+   private function setparam ($statments, $key, $values){
+   	    $statments->bindparam($key, $values);
+   }
+   public function query ($rawquery, $params = array()){
+	    $stmt = $this ->conn->prepare($rawquery);
+	 
+   $this ->setparams($stmt, $params);
+
+        $stmt->execute();
+        return $stmt;
+
+}
+
+  public function select ($rawquery,$params=array()){
+
+  	  $stmt = $this ->query($rawquery, $params);
+  	  return $stmt->fetchALL(PDO::FETCH_ASSOC);
+  }
+}
+?>
